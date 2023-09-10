20310e8398bbc49985f893f2ece4b7dc57ede180
Tyler Kibble <tylerkibble1234@gmail.com>
2023-09-09 17:22:47 -0700

    security updates

--- app/routes/api/user/user.js
+++ app/routes/api/user/user.js
@@ -2,10 +2,18 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../../../controllers/userController");
 const auth = require("../../../config/auth")
+const rateLimit = require("express-rate-limit");
 
-router.get("/me", auth,userController.getUserDetails);
-router.post("/register", userController.registerNewUser);
-router.post("/login", userController.loginUser);
-// router.get("/me", userController.getUserDetails);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+
+router.get("/me", auth, limiter, userController.getUserDetails);
+
+router.post("/register", limiter, userController.registerNewUser);
+router.post("/login", limiter, userController.loginUser);
+
 
 module.exports = router;
