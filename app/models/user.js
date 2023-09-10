cf6f2d0461f046d2b3b4a7fd84e032435aa8f423
Tyler Kibble <tylerkibble1234@gmail.com>
2023-09-09 17:00:14 -0700

    Hard-coded credentials fix

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const secret = process.env.TOKEN_SECRET;
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -25,7 +26,7 @@ const userSchema = mongoose.Schema({
 });
 
 //this method will hash the password before saving the user model
-userSchema.pre("save", async function(next) {
+userSchema.pre("save", async function (next) {
   const user = this;
   if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
@@ -34,10 +35,12 @@ userSchema.pre("save", async function(next) {
 });
 
 //this method generates an auth token for the user
-userSchema.methods.generateAuthToken = async function() {
+userSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = jwt.sign({ _id: user._id, name: user.name, email: user.email },
-  "secret");
+  const token = jwt.sign({
+    exp: Math.floor(Date.now() / 1000) + (60 * 60),
+    data: { _id: user._id, name: user.name, email: user.email },
+  }, secret);
   user.tokens = user.tokens.concat({ token });
   await user.save();
   return token;
